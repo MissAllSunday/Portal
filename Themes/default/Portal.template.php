72e1f676b5d99e9cc4c859b1a2a00e1b7a9d1cbd
Suki <suki@missallsunday.com>
2015-07-30 15:18:17 -0500

    Add our own menu template
    Signed-off-by: Suki <suki@missallsunday.com>

--- Themes/default/Portal.template.php
+++ Themes/default/Portal.template.php
@@ -10,12 +10,40 @@
 
 function template_portal_above()
 {
+	global $context;
 
-}
+	echo '
+	<div class="portal mdl-layout mdl-js-layout has-drawer is-upgraded">
+		<header class="mdl-layout__header mdl-layout__header--transparent">
+			<div class="mdl-layout__header-row">
+				<!-- Title -->
+				<span class="mdl-layout-title">', $context['page_title'] ,'</span>
+				<!-- Add spacer, to align navigation to the right -->
+				<div class="mdl-layout-spacer"></div>
+				<!-- Navigation -->
+				<nav class="mdl-navigation">
+				<a class="mdl-navigation__link" href="">Forum</a>
+				<a class="mdl-navigation__link" href="">Profile</a>
+				<a class="mdl-navigation__link" href="">Other Link</a>
+				<a class="mdl-navigation__link" href="">Link</a>
+				</nav>
+			</div>
+		</header>
+		<div class="mdl-layout__drawer">
+			<nav class="mdl-navigation">';
 
-function template_portal_below()
-{
+	// Note: Menu markup has been cleaned up to remove unnecessary spans and classes.
+	foreach ($context['menu_buttons'] as $act => $button)
+	{
+		echo '
+				<a class="mdl-navigation__link ', $button['active_button'] ? ' active' : '', '" href="', $button['href'], '"', isset($button['target']) ? ' target="' . $button['target'] . '"' : '', '>
+					', $button['title'], '
+				</a>';
+	}
 
+	echo '
+		</nav>
+	</div>';
 }
 
 function template_portal_main()
@@ -23,122 +51,125 @@ function template_portal_main()
 	global $context;
 
 	echo '
-		<div class="portal mdl-layout mdl-js-layout has-drawer is-upgraded">
-			', template_menu() ,'
-			<main class="mdl-layout__content">
-				<div class="portal__posts mdl-grid">
-					<div class="mdl-card coffee-pic mdl-cell mdl-cell--8-col">
-						<div class="mdl-card__media mdl-color-text--grey-50">
-							<h3><a href="entry.html">Coffee Pic</a></h3>
-						</div>
-						<div class="mdl-card__supporting-text meta mdl-color-text--grey-600">
-							<div class="minilogo"></div>
-							<div>
-								<strong>The Newist</strong>
-								<span>2 days ago</span>
-							</div>
+		<main class="mdl-layout__content">
+			<div class="portal__posts mdl-grid">
+				<div class="mdl-card coffee-pic mdl-cell mdl-cell--8-col">
+					<div class="mdl-card__media mdl-color-text--grey-50">
+						<h3><a href="entry.html">Coffee Pic</a></h3>
+					</div>
+					<div class="mdl-card__supporting-text meta mdl-color-text--grey-600">
+						<div class="minilogo"></div>
+						<div>
+							<strong>The Newist</strong>
+							<span>2 days ago</span>
 						</div>
 					</div>
-					<div class="mdl-card something-else mdl-cell mdl-cell--8-col mdl-cell--4-col-desktop">
-						<button class="mdl-button mdl-js-ripple-effect mdl-js-button mdl-button--fab mdl-color--accent">
-							<i class="material-icons mdl-color-text--white" role="presentation">add</i>
-							<span class="visuallyhidden">add</span>
-						</button>
-						<div class="mdl-card__media mdl-color--white mdl-color-text--grey-600">
-							<img src="images/logo.png">
-							+1,337
-						</div>
-						<div class="mdl-card__supporting-text meta meta--fill mdl-color-text--grey-600">
-							<div>
-								<strong>The Newist</strong>
-							</div>
-							<ul class="mdl-menu mdl-js-menu mdl-menu--bottom-right" for="menubtn">
-								<li class="mdl-menu__item mdl-js-ripple-effect">About</li>
-								<li class="mdl-menu__item mdl-js-ripple-effect">Message</li>
-								<li class="mdl-menu__item mdl-js-ripple-effect">Favorite</li>
-								<li class="mdl-menu__item mdl-js-ripple-effect">Search</li>
-							</ul>
-							<button id="menubtn" class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--icon">
-								<i class="material-icons" role="presentation">more_vert</i>
-								<span class="visuallyhidden">show menu</span>
-							</button>
-						</div>
-					</div>
-					<div class="mdl-card on-the-road-again mdl-cell mdl-cell--12-col">
-						<div class="mdl-card__media mdl-color-text--grey-50">
-							<h3><a href="entry.html">On the road again</a></h3>
-						</div>
-						<div class="mdl-color-text--grey-600 mdl-card__supporting-text">
-							Enim labore aliqua consequat ut quis ad occaecat aliquip incididunt. Sunt nulla eu enim irure enim nostrud aliqua consectetur ad consectetur sunt ullamco officia. Ex officia laborum et consequat duis.
-						</div>
-						<div class="mdl-card__supporting-text meta mdl-color-text--grey-600">
-							<div class="minilogo"></div>
-							<div>
-								<strong>The Newist</strong>
-								<span>2 days ago</span>
-							</div>
-						</div>
-					</div>
-					<div class="mdl-card amazing mdl-cell mdl-cell--12-col">
-						<div class="mdl-card__title mdl-color-text--grey-50">
-							<h3 class="quote"><a href="entry.html">I couldn’t take any pictures but this was an amazing thing…</a></h3>
-						</div>
-						<div class="mdl-card__supporting-text mdl-color-text--grey-600">
-							Enim labore aliqua consequat ut quis ad occaecat aliquip incididunt. Sunt nulla eu enim irure enim nostrud aliqua consectetur ad consectetur sunt ullamco officia. Ex officia laborum et consequat duis.
-						</div>
-						<div class="mdl-card__supporting-text meta mdl-color-text--grey-600">
-							<div class="minilogo"></div>
-							<div>
-								<strong>The Newist</strong>
-								<span>2 days ago</span>
-							</div>
-						</div>
-					</div>
-					<div class="mdl-card shopping mdl-cell mdl-cell--12-col">
-						<div class="mdl-card__media mdl-color-text--grey-50">
-							<h3><a href="entry.html">Shopping</a></h3>
-						</div>
-						<div class="mdl-card__supporting-text mdl-color-text--grey-600">
-							Enim labore aliqua consequat ut quis ad occaecat aliquip incididunt. Sunt nulla eu enim irure enim nostrud aliqua consectetur ad consectetur sunt ullamco officia. Ex officia laborum et consequat duis.
-						</div>
-						<div class="mdl-card__supporting-text meta mdl-color-text--grey-600">
-							<div class="minilogo"></div>
-							<div>
-								<strong>The Newist</strong>
-								<span>2 days ago</span>
-							</div>
-						</div>
-					</div>
-					<nav class="demo-nav mdl-cell mdl-cell--12-col">
-						<div class="section-spacer"></div>
-						<a href="entry.html" class="demo-nav__button" title="show more">
-							More
-							<button class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--icon">
-								<i class="material-icons" role="presentation">arrow_forward</i>
-							</button>
-						</a>
-					</nav>
 				</div>
-				<footer class="mdl-mini-footer">
-					<div class="mdl-mini-footer--left-section">
-						<button class="mdl-mini-footer--social-btn social-btn social-btn__twitter">
-							<span class="visuallyhidden">Twitter</span>
-						</button>
-						<button class="mdl-mini-footer--social-btn social-btn social-btn__blogger">
-							<span class="visuallyhidden">Facebook</span>
-						</button>
-						<button class="mdl-mini-footer--social-btn social-btn social-btn__gplus">
-							<span class="visuallyhidden">Google Plus</span>
+				<div class="mdl-card something-else mdl-cell mdl-cell--8-col mdl-cell--4-col-desktop">
+					<button class="mdl-button mdl-js-ripple-effect mdl-js-button mdl-button--fab mdl-color--accent">
+						<i class="material-icons mdl-color-text--white" role="presentation">add</i>
+						<span class="visuallyhidden">add</span>
+					</button>
+					<div class="mdl-card__media mdl-color--white mdl-color-text--grey-600">
+						<img src="images/logo.png">
+						+1,337
+					</div>
+					<div class="mdl-card__supporting-text meta meta--fill mdl-color-text--grey-600">
+						<div>
+							<strong>The Newist</strong>
+						</div>
+						<ul class="mdl-menu mdl-js-menu mdl-menu--bottom-right" for="menubtn">
+							<li class="mdl-menu__item mdl-js-ripple-effect">About</li>
+							<li class="mdl-menu__item mdl-js-ripple-effect">Message</li>
+							<li class="mdl-menu__item mdl-js-ripple-effect">Favorite</li>
+							<li class="mdl-menu__item mdl-js-ripple-effect">Search</li>
+						</ul>
+						<button id="menubtn" class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--icon">
+							<i class="material-icons" role="presentation">more_vert</i>
+							<span class="visuallyhidden">show menu</span>
 						</button>
 					</div>
-					<div class="mdl-mini-footer--right-section">
-						<button class="mdl-mini-footer--social-btn social-btn__share">
-							<i class="material-icons" role="presentation">share</i>
-							<span class="visuallyhidden">share</span>
-						</button>
+				</div>
+				<div class="mdl-card on-the-road-again mdl-cell mdl-cell--12-col">
+					<div class="mdl-card__media mdl-color-text--grey-50">
+						<h3><a href="entry.html">On the road again</a></h3>
 					</div>
-				</footer>
-			</main>
-			<div class="mdl-layout__obfuscator"></div>
-		</div>';
+					<div class="mdl-color-text--grey-600 mdl-card__supporting-text">
+						Enim labore aliqua consequat ut quis ad occaecat aliquip incididunt. Sunt nulla eu enim irure enim nostrud aliqua consectetur ad consectetur sunt ullamco officia. Ex officia laborum et consequat duis.
+					</div>
+					<div class="mdl-card__supporting-text meta mdl-color-text--grey-600">
+						<div class="minilogo"></div>
+						<div>
+							<strong>The Newist</strong>
+							<span>2 days ago</span>
+						</div>
+					</div>
+				</div>
+				<div class="mdl-card amazing mdl-cell mdl-cell--12-col">
+					<div class="mdl-card__title mdl-color-text--grey-50">
+						<h3 class="quote"><a href="entry.html">I couldn’t take any pictures but this was an amazing thing…</a></h3>
+					</div>
+					<div class="mdl-card__supporting-text mdl-color-text--grey-600">
+						Enim labore aliqua consequat ut quis ad occaecat aliquip incididunt. Sunt nulla eu enim irure enim nostrud aliqua consectetur ad consectetur sunt ullamco officia. Ex officia laborum et consequat duis.
+					</div>
+					<div class="mdl-card__supporting-text meta mdl-color-text--grey-600">
+						<div class="minilogo"></div>
+						<div>
+							<strong>The Newist</strong>
+							<span>2 days ago</span>
+						</div>
+					</div>
+				</div>
+				<div class="mdl-card shopping mdl-cell mdl-cell--12-col">
+					<div class="mdl-card__media mdl-color-text--grey-50">
+						<h3><a href="entry.html">Shopping</a></h3>
+					</div>
+					<div class="mdl-card__supporting-text mdl-color-text--grey-600">
+						Enim labore aliqua consequat ut quis ad occaecat aliquip incididunt. Sunt nulla eu enim irure enim nostrud aliqua consectetur ad consectetur sunt ullamco officia. Ex officia laborum et consequat duis.
+					</div>
+					<div class="mdl-card__supporting-text meta mdl-color-text--grey-600">
+						<div class="minilogo"></div>
+						<div>
+							<strong>The Newist</strong>
+							<span>2 days ago</span>
+						</div>
+					</div>
+				</div>
+				<nav class="demo-nav mdl-cell mdl-cell--12-col">
+					<div class="section-spacer"></div>
+					<a href="entry.html" class="demo-nav__button" title="show more">
+						More
+						<button class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--icon">
+							<i class="material-icons" role="presentation">arrow_forward</i>
+						</button>
+					</a>
+				</nav>
+			</div>
+			<footer class="mdl-mini-footer">
+				<div class="mdl-mini-footer--left-section">
+					<button class="mdl-mini-footer--social-btn social-btn social-btn__twitter">
+						<span class="visuallyhidden">Twitter</span>
+					</button>
+					<button class="mdl-mini-footer--social-btn social-btn social-btn__blogger">
+						<span class="visuallyhidden">Facebook</span>
+					</button>
+					<button class="mdl-mini-footer--social-btn social-btn social-btn__gplus">
+						<span class="visuallyhidden">Google Plus</span>
+					</button>
+				</div>
+				<div class="mdl-mini-footer--right-section">
+					<button class="mdl-mini-footer--social-btn social-btn__share">
+						<i class="material-icons" role="presentation">share</i>
+						<span class="visuallyhidden">share</span>
+					</button>
+				</div>
+			</footer>
+		</main>';
+}
+
+function template_portal_below()
+{
+	echo '
+		<div class="mdl-layout__obfuscator"></div>
+	</div>';
 }
